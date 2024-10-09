fccafec67853b04657a44a1eaca2b80fc39d87ab
SigneNilsson <nilsson.signe@gmail.com>
2024-10-09 14:18:27 +0200

    Added burgers and ingredients

--- Containers/burger_orderer/app.py
+++ Containers/burger_orderer/app.py
@@ -5,9 +5,8 @@ import os
 
 app = Flask(__name__)
 
-staticBurgers= [{"name":"fettburgare"},
-                {"name":"gnuttburgare"},
-                {"name":"isterburgare"},
+staticBurgers= [{"name":"Mostafaburgare"},
+                {"name":"Eidamburgare"},
                 {"name":"Signeburgare"}]
 
 def getBurgers():
