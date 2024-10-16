4083ed9f0477ff3001f0b04248e2b143cb6d46af
SigneNilsson <nilsson.signe@gmail.com>
2024-10-16 22:05:47 +0200

    Added an automated test to ensure there are three burgers available. Commented out code that does not work as to change it tomorrow

--- Containers/burger_orderer/test_buy2.py
+++ Containers/burger_orderer/test_buy2.py
@@ -1,13 +1,16 @@
 import pytest
-from app import buy2
+from app import buy2, getBurgers
 
-def buy2():
+""" def buy2():
     return getBurgers()
 
 
 @pytest.buy2
 def buy2():
-    assert buy2(getBurgers)
+    assert buy2(getBurgers)"""
     
     
+def test_getBurgers():
+    burgers = getBurgers()
 
+    assert(len(burgers) == 3)
